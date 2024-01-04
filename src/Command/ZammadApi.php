f242eb3cf745a908d48c0218bb178c522faea1a6
castard75 <castard75@gmail.com>
2024-01-04 11:28:02 +0100

    entité task - ajout de nouveau champs"

--- src/Command/ZammadApi.php
+++ src/Command/ZammadApi.php
@@ -9,8 +9,9 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpClient\HttpClient;
+use App\Entity\Task;
 use ZammadAPIClient\Client;
-
+use ZammadAPIClient\ResourceType;
 
 // the name of the command is what users type after "php bin/console"
 #[AsCommand(name: 'app:zammad')]
@@ -23,13 +24,7 @@ class CreateUserCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        // ... put here the code to create the user
-
-        // this method must return an integer number with the "exit status code"
-        // of the command. You can also use these constants to make code more readable
-
-        // return this if there was no problem running the command
-        // (it's equivalent to returning int(0))
+        $this->getDatas();
         return Command::SUCCESS;
 
         // or return this if some error happened during the execution
@@ -43,8 +38,10 @@ class CreateUserCommand extends Command
 
 
 public function getDatas(){
+  
+    // $ListeTask = $this->em->getRepository(Task::class)->findAll();
 
-
+    //Connexion du client
     $client = new Client([
         'url'           => 'https://myzammad.com', // URL to your Zammad installation
         'username'      => 'myuser@myzammad.com',  // Username to use for authentication
@@ -53,6 +50,32 @@ public function getDatas(){
         // 'debug'         => true,                // Enables debug output
         // 'verify'        => true,                // Enabled SSL verification. You can also give a path to a CA bundle file. Default is true.
     ]);
+
+  
+    
+
+
+    $ticket = $client->resource( ResourceType::TICKET );
+    $ticket->setValue( 'title', 'My new ticket' );
+    $ticket->setValue( 'group', 'exemple' );
+    $ticket->setValue( 'customer', 'My new ticket' );
+    $ticket->setValue( 'article', 'My new ticket' );
+    // ...
+    // Set additional values
+    // 
+    // {
+    //     "title": "Help me!",
+    //     "group": "2nd Level",
+    //     "customer": "david@example.com",
+    //     "article": {
+    //        "subject": "My subject",
+    //        "body": "I am a message!",
+    //        "type": "note",
+    //        "internal": false
+    //     }
+    //  }
+    $ticket->save(); // Will create a new ticket object in Zammad
+
 
 }
 
--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -5,12 +5,21 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
 use App\Repository\TaskRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: TaskRepository::class)]
 #[ApiResource]
 class Task
 {
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime('now');
+        $this->updatedAt = new \DateTime('now');
+    }
+
+
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
@@ -33,6 +42,36 @@ class Task
 
     #[ORM\Column(length: 100, nullable: true)]
     private ?string $client = null;
+
+    #[ORM\Column(length: 150, nullable: true)]
+    private ?string $subject = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $type = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $internal = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $groupZad = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $deletedAt = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $updatedAt = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $status = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $transfert = null;
 
     public function getId(): ?int
     {
@@ -107,6 +146,126 @@ class Task
     public function setClient(?string $client): static
     {
         $this->client = $client;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(?string $subject): static
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(?string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function isInternal(): ?bool
+    {
+        return $this->internal;
+    }
+
+    public function setInternal(?bool $internal): static
+    {
+        $this->internal = $internal;
+
+        return $this;
+    }
+
+    public function getGroupZad(): ?string
+    {
+        return $this->groupZad;
+    }
+
+    public function setGroupZad(?string $groupZad): static
+    {
+        $this->groupZad = $groupZad;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getDeletedAt(): ?\DateTimeImmutable
+    {
+        return $this->deletedAt;
+    }
+
+    public function setDeletedAt(?\DateTimeImmutable $deletedAt): static
+    {
+        $this->deletedAt = $deletedAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeImmutable
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeImmutable $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(?\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?int $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getTransfert(): ?int
+    {
+        return $this->transfert;
+    }
+
+    public function setTransfert(?int $transfert): static
+    {
+        $this->transfert = $transfert;
 
         return $this;
     }
